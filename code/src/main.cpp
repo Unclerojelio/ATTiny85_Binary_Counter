895115753b35c63bdc8fd8d6dfea31e0066391b2
Roger Banks <roger_banks@mac.com>
2021-08-15 11:52:33 -0500

    First successful compile and upload.

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,9 +1,25 @@
+// This sketch outputs a binary coded hexadecimal value out to the pins of an
+// ATTiny85.
+//
+// Roger Banks
+// 15 Aug 2021
+//
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  // Set pins to output
+  pinMode(PCINT0, OUTPUT);
+  pinMode(PCINT1, OUTPUT);
+  pinMode(PCINT2, OUTPUT);
+  pinMode(PCINT3, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(byte x = 0; x < 16; x++) {
+    digitalWrite(PCINT0, x & B0001);
+    digitalWrite(PCINT1, x & B0010);
+    digitalWrite(PCINT2, x & B0100);
+    digitalWrite(PCINT3, x & B1000);
+    delay(1000);
+  }
 }
